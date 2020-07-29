bed87a7e7aa3910c1a3292e7e70244e4ea34ce6e
KerNioZz <64327328+KerNioZz@users.noreply.github.com>
2020-07-29 20:40:06 +0200

    Add files via upload

---
+++ chat.php
@@ -0,0 +1,60 @@
+<?php 
+include 'class.chatbot.php';
+$chatbot = new Chatbot;
+
+$db_host = "";
+$db_name = "";
+$db_user = "";
+$db_pass = "";
+
+$pdo = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8", "{db_user}", "{db_pass}");
+
+
+
+if (isset($_POST["pseudo"]) && !empty($_POST["pseudo"]) && isset($_POST["message"]) && !empty($_POST["message"])) {
+
+	$pseudo = htmlspecialchars($_POST["pseudo"]);
+
+	$message = nl2br(htmlspecialchars($_POST["message"]));
+
+	$insmsg = $pdo->prepare("INSERT INTO chat(pseudo, message) VALUES (:pseudo, :message)");
+	$insmsg->execute(array(
+		"pseudo" => $pseudo,
+		"message" => $message
+	));
+	$message = $chatbot->sayHello($message);
+	$message = $chatbot->whereAreWe($message);
+	$message = $chatbot->speakRussian($message);
+}
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Chat avec Chatbot</title>
+</head>
+<body>
+	<div>
+		<h1>Chat avec Chatbot</h1>
+		<form method="POST" action="">
+			<fieldset>
+				<legend>Coordonnées</legend>
+				<label for="pseudo">Pseudo</label><br />
+				<input type="text" name="pseudo" /><br />
+				<label for="message">Message</label><br />
+				<textarea name="message"></textarea><br />
+				<input type="submit" name="submit">
+			</fieldset>
+		</form>
+	</div>
+	<div>
+		<h2>Messages</h2>
+		<p>
+			<?php $req_msgs = $pdo->query("SELECT * FROM chat ORDER BY id DESC");
+			while ($show_msgs = $req_msgs->fetch()) { ?>
+			 	<b><?= $show_msgs["pseudo"]?> :</b> <?= $show_msgs["message"]?><br />
+			<?php } ?>
+		</p>
+	</div>
+</body>
+</html>
---
+++ class.chatbot.php
@@ -0,0 +1,46 @@
+<?php 
+/**
+* @author Kévin BENTO, 17-year-old student
+* @param string db_host, db_name, db_user, db_pass Connection parameters to your Database
+*/
+class Chatbot {
+
+	public function sayHello() {
+		$db_host = "";
+		$db_name = "";
+		$db_user = "";
+		$db_pass = "";
+		$pdo = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8", "{db_user}", "{db_pass}");
+		$hello = preg_match("#Hello bot!#", $_POST['message']);
+		if ($hello) {
+			$pseudo_bot = "Bot";
+			$message_bot = "Hello!";
+
+			$ins_bot = $pdo->prepare("INSERT INTO chat(pseudo, message) VALUES(:pseudo_bot, :message_bot)");
+			$ins_bot->execute(array(
+				"pseudo_bot" => $pseudo_bot,
+				"message_bot" => $message_bot
+			));
+		}
+	}
+
+	/* 
+	public function chatBot() {
+		$pdo = new PDO("mysql:host=YOUR_DATABASE_HOST;dbname=YOUR_DATABASE_NAME;charset=utf8", "YOUR_USERNAME", "YOUR_PASSWORD");
+		$place = preg_match("#String to search#", $_POST['message']);
+		if ($place) {
+			$pseudo_bot = "Bot";
+			$message_bot = "String to print";
+
+			$ins_bot = $pdo->prepare("INSERT INTO chat(pseudo, message) VALUES(:pseudo_bot, :message_bot)");
+			$ins_bot->execute(array(
+				"pseudo_bot" => $pseudo_bot,
+				"message_bot" => $message_bot
+			));
+		}
+	}
+	*/
+}
+
+
+?>
