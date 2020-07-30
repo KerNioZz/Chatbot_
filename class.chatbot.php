6fc7d71601fad3bba6239ad9fe5d60065fedd164
KerNioZz <64327328+KerNioZz@users.noreply.github.com>
2020-07-30 07:48:52 +0200

    Update class.chatbot.php

--- class.chatbot.php
+++ class.chatbot.php
@@ -26,6 +26,10 @@ class Chatbot {
 
 	/* 
 	public function chatBot() {
+		$db_host = "";
+		$db_name = "";
+		$db_user = "";
+		$db_pass = "";
 		$pdo = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8", "{db_user}", "{db_pass}");
 		$place = preg_match("#String to search#", $_POST['message']);
 		if ($place) {
